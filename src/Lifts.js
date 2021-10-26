1d890c907e04490613055f50bca0d104321a4984
Marcin Bieszka <marcinbieszka@Marcins-MacBook-Air.local>
2021-10-26 17:43:04 +0200

    Fix RAL and fov

--- src/Lifts.js
+++ src/Lifts.js
@@ -154,21 +154,21 @@ class WelcomeToVR extends React.Component {
         />
         <MaterialChange
           pos1={this.changePos(" 0 0 0")}
-          pos2={this.changePos(" 0 0 -20")}
-          pos3={this.changePos(" 0 0 -40")}
-          pos4={this.changePos(" 0 0 -60")}
-          pos5={this.changePos(" 20 0 0 ")}
-          pos6={this.changePos(" 20 0 -20")}
-          pos7={this.changePos(" 20 0 -40")}
-          pos8={this.changePos(" 20 0 -60")}
-          pos9={this.changePos(" 40 0 0")}
-          pos10={this.changePos("40 0 -20")}
-          pos11={this.changePos("40 0 -40")}
-          pos12={this.changePos("40 0 -60")}
-          pos13={this.changePos("60 0 0")}
-          pos14={this.changePos("60 0 -20")}
-          pos15={this.changePos("60 0 -40")}
-          pos16={this.changePos("60 0 -60")}
+          pos2={this.changePos(" 0 0 -200")}
+          pos3={this.changePos(" 0 0 -400")}
+          pos4={this.changePos(" 0 0 -600")}
+          pos5={this.changePos(" 200 0 0 ")}
+          pos6={this.changePos(" 200 0 -200")}
+          pos7={this.changePos(" 200 0 -400")}
+          pos8={this.changePos(" 200 0 -600")}
+          pos9={this.changePos(" 400 0 0")}
+          pos10={this.changePos("400 0 -200")}
+          pos11={this.changePos("400 0 -400")}
+          pos12={this.changePos("400 0 -600")}
+          pos13={this.changePos("600 0 0")}
+          pos14={this.changePos("600 0 -200")}
+          pos15={this.changePos("600 0 -400")}
+          pos16={this.changePos("600 0 -600")}
         />
         <div style={{ minWidth: "100vw", minHeight: "100vh" }}>
           <Scene
@@ -205,25 +205,25 @@ class WelcomeToVR extends React.Component {
               <img id="caropattern" src={CaroPatternPlatform} alt="" />
               <img id="ralfoil" src={RALFoilPlatform} alt="" />
             </a-assets>
-            <a-sky position="0 0 0" radius="10" src="#ral9006" />
-            <a-sky position="0 0 -20" radius="10" src="#darkoak" />
-            <a-sky position="0 0 -40" radius="10" src="#birchoak" />
-            <a-sky position="0 0 -60" radius="10" src="#blackfoilpattern" />
+            <a-sky position="0 0 0" radius="100" src="#ral9006" />
+            <a-sky position="0 0 -200" radius="100" src="#darkoak" />
+            <a-sky position="0 0 -400" radius="100" src="#birchoak" />
+            <a-sky position="0 0 -600" radius="100" src="#blackfoilpattern" />
 
-            <a-sky position="20 0 0" radius="10" src="#spruceoak" />
-            <a-sky position="20 0 -20" radius="10" src="#oak" />
-            <a-sky position="20 0 -40" radius="10" src="#acaciaoak" />
-            <a-sky position="20 0 -60" radius="10" src="#whitefoilpattern" />
+            <a-sky position="200 0 0" radius="100" src="#spruceoak" />
+            <a-sky position="200 0 -200" radius="100" src="#oak" />
+            <a-sky position="200 0 -400" radius="100" src="#acaciaoak" />
+            <a-sky position="200 0 -600" radius="100" src="#whitefoilpattern" />
 
-            <a-sky position="40 0 0" radius="10" src="#blackskin" />
-            <a-sky position="40 0 -20" radius="10" src="#whiteoak" />
-            <a-sky position="40 0 -40" radius="10" src="#redoak" />
-            <a-sky position="40 0 -60" radius="10" src="#materialwhiteskin" />
+            <a-sky position="400 0 0" radius="100" src="#blackskin" />
+            <a-sky position="400 0 -200" radius="100" src="#whiteoak" />
+            <a-sky position="400 0 -400" radius="100" src="#redoak" />
+            <a-sky position="400 0 -600" radius="100" src="#materialwhiteskin" />
 
-            <a-sky position="60 0 0" radius="10" src="#lightoak" />
-            <a-sky position="60 0 -20" radius="10" src="#sheetpattern" />
-            <a-sky position="60 0 -40" radius="10" src="#caropattern" />
-            <a-sky position="60 0 -60" radius="10" src="#ralfoil" />
+            <a-sky position="600 0 0" radius="100" src="#lightoak" />
+            <a-sky position="600 0 -200" radius="100" src="#sheetpattern" />
+            <a-sky position="600 0 -400" radius="100" src="#caropattern" />
+            <a-sky position="600 0 -600" radius="100" src="#ralfoil" />
             <a-entity position={this.state.currentPos} rotation="0 90 0">
               <a-camera
                 fov="80"
